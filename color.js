dd3147b0f058ff0ab170f4adc589bf49944906cc
tutich <pkoinange02@gmail.com>
2023-09-01 21:39:47 +0300

    JS functions

--- color.js
+++ color.js
@@ -3,13 +3,14 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
+    // get random number between 0 - 3
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-    console.log(randomNumber);
+    console.log(randomNumber)
 });
 
 function getRandomNumber() {
-    return Math.floor(math.random() * colors.length);
+    return Math.floor(Math.random() * colors.length);
 
 }
